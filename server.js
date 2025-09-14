6b1137a17c0fe8acfc520b156510320f82e588e4
Kralik N <kraaliknorbert@gmail.com>
2025-09-14 20:06:04 +0200

    update server.js

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 8080;   // Azure adja
+const host = "0.0.0.0";                           // ne csak localhost
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-    createServer((req, res) => {
+    const server = createServer((req, res) => {
         const parsedUrl = parse(req.url, true);
         handle(req, res, parsedUrl);
-    }).listen(process.env.PORT || 3000, (err) => {
+    });
+
+    server.listen(port, host, (err) => {
         if (err) throw err;
-        console.log(`> Ready on http://localhost:${port}`);
+        console.log(`> Ready on http://${host}:${port} (NODE_ENV=${process.env.NODE_ENV})`);
     });
 });
